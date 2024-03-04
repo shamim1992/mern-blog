e76055c916022097508da3ad5a163016b1f2ff6a
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-03-04 15:19:31 +0530

    mern blog project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+import Projects from './pages/Projects'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
