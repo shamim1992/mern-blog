81d9bed119b104e25a360b3e0df6ec95c5b0b988
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-03-04 21:09:49 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoute.js';
+
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
 });
 
-
+app.use('/api/user', userRoutes);
 app.listen(process.env.PORT, () => console.log('Connected to', process.env.PORT));
