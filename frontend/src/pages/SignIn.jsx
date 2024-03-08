34525db3aafde66f7480238ddd2cd5bbe6eaff9a
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-03-08 12:10:27 +0530

    updated login page with backend features

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import { data } from 'autoprefixer'
+import { Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function SignIn() {
+    const [formData, setFormData] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(false)
+    const navigate = useNavigate()
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (!formData.password || !formData.email) {
+            setErrorMessage(true)
+            return setErrorMessage("Please fill out all required fields")
+        }
+        console.log(formData)
+        try {
+            setLoading(true)
+            setErrorMessage(null)
+            const res = await fetch(`/api/auth/signin`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            if (data.success === false) {
+                return setErrorMessage(data.message);
+            }
+            setLoading(false)
+            if (res.ok === true) {
+                navigate('/')
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+            setLoading(false)
+        }
+    }
     return (
-        <div>SignIn</div>
+        <div className='min-h-screen mt-20'>
+            <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+                {/* Left side div */}
+                <div className='flex-1'>
+                    <Link
+                        to='/'
+                        className=' font-bold dark:text-white text-4xl'
+                    >
+                        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                            NE
+                        </span>
+                        Sportz
+                    </Link>
+                    <p className='text-sm mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, tenetur!</p>
+                </div>
+
+
+                {/* Right side div */}
+                <div className='flex-1'>
+                    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+
+                        <div className='mb-3'>
+                            <Label value='Your Email'></Label>
+                            <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} />
+                        </div>
+                        <div className='mb-3'>
+                            <Label value='Your Password'></Label>
+                            <TextInput type='password' placeholder='*********' id='password' onChange={handleChange} />
+                        </div>
+                        <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                            {loading ? (<><Spinner size='sm' /> <span className='pl-3'>Loading</span></>) : 'Sign In'}
+                        </Button>
+                    </form>
+
+                    <div className='flex gap-2 text-sm mt-5'>
+                        <span>Don't have an account? </span><Link to={'/signup'} className='text-blue-500'>Sign up</Link>
+                    </div>
+                    {errorMessage && <p className='text-red-500 text-sm'>{errorMessage}</p>}
+                </div>
+            </div>
+        </div>
     )
 }
 
